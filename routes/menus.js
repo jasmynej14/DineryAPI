671e67e576e02e13ffa6ec6f0da8a7d6d71b4d3b
jasmynej14 <jasmynej14@brandeis.edu>
2021-10-05 10:26:51 -0400

    post menus

--- routes/menus.js
+++ routes/menus.js
@@ -2,9 +2,25 @@ var express = require('express');
 var router = express.Router();
 const asyncHandler = require('express-async-handler')
 const Menu = require('../models/menuModel')
-
+const Item = require('../models/itemModel')
 router.get('/',asyncHandler(async (req,res)=>{
     const allMenus = await Menu.find({}).populate('restaurant')
     res.send(allMenus)
+}))
+
+router.get('/:id/items', asyncHandler(async (req,res) => {
+    const fullMenu = {}
+    const menu = await Menu.findById(req.params.id)
+    const items = await Item.find({menu:menu})
+    fullMenu.menu = menu
+    fullMenu.items = items
+    res.json(fullMenu)
+}))
+
+router.post('/',asyncHandler(async (req,res) => {
+    const menu = {name:req.body.name,restaurant:req.body.restaurant}
+    Menu.create(menu,function(err,createdMenu){
+        res.json(createdMenu)
+    })
 }))
 module.exports = router;
